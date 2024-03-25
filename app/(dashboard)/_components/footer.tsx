b8b966e008aeea0a682324309121d559d96dc5d7
chaimaabouhdid <chaimaabouhdid1@gmail.com>
2024-03-25 22:27:23 +0000

    footer design

--- app/(dashboard)/_components/footer.tsx
+++ app/(dashboard)/_components/footer.tsx
@@ -9,9 +9,8 @@ import {
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-between py-10 w-full gap-16 items-center 
-        overflow-hidden border rounded-lg p-3 h-full
-         bg-white text-slate-900">
+        <footer className="flex flex-col justify-between py-10 w-full gap-16 items-center border p-3 h-full bg-slate-50
+         text-slate-900">
             <h1 className="text-4xl font-bold text-emerald-600">IT Torch</h1>
             <div className="flex flex-col gap-4 md:flex-row md:gap-8">
                 <Link href="/">Dashboard</Link>
@@ -22,16 +21,16 @@ export default function Footer() {
             </div>
 
             <div className="flex gap-4">
-                <Link href="#">
+                <Link href="https://www.linkedin.com/signup?trk=guest_homepage-basic_nav-header-join">
                     <FaLinkedinIn size={20} />
                 </Link>
-                <Link href="#">
+                <Link href="https://twitter.com/">
                     <FaTwitter size={20} />
                 </Link>
-                <Link href="#">
+                <Link href="https://www.facebook.com/">
                     <FaFacebook size={20} />
                 </Link>
-                <Link href="#">
+                <Link href="https://www.instagram.com/">
                     <FaInstagram size={20} />
                 </Link>
             </div>
